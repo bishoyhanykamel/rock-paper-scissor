1b2a86a1cbb31de765c88e31946970a70a576721
Bishoy Hany Kamel <bishoyhany31@example.com>
2022-09-18 23:08:17 +0200

    Add random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function getComputerChoice() {
+    let randomValue = Math.floor(Math.random() * 100) + 1;
+    if (randomValue <= 30) return 'Rock';
+    if (randomValue <= 60) return 'Paper';
+    return 'Scissors';
+}
+
+
