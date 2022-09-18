aabc6965ed65e778427e795b1131487c071c00b5
Bishoy Hany Kamel <bishoyhany31@example.com>
2022-09-18 23:31:20 +0200

    Add getPlayerChoice

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 "use strict";
 
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 let getComputerChoice = function() {
     let randomValue = Math.floor(Math.random() * 100) + 1;
-    if (randomValue <= 30) return 'Rock';
-    if (randomValue <= 60) return 'Paper';
-    return 'Scissors';
+    if (randomValue <= 30) return ROCK;
+    if (randomValue <= 60) return PAPER;
+    return SCISSORS;
 }
 
-
+function getPlayerChoice() {
+    let userChoice = undefined;
+    do {
+        userChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors'").toLowerCase();
+    } while (userChoice !== ROCK && userChoice !== PAPER && userChoice !== SCISSORS);
+}
