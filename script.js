4d0e87ea6e2a6312df3d4a2ec28edb5efdc3aa8f
Bishoy Hany Kamel <bishoyhany31@example.com>
2022-09-18 23:09:54 +0200

    Change getComputerChoice to Function Expression

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function getComputerChoice() {
+let getComputerChoice = function() {
     let randomValue = Math.floor(Math.random() * 100) + 1;
     if (randomValue <= 30) return 'Rock';
     if (randomValue <= 60) return 'Paper';
