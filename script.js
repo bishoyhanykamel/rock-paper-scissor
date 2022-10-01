9a708a6cda74cd0d12d4008a52b4ee26f3011951
Bishoy Hany <bishoyhany31@hotmail.com>
2022-10-01 18:21:38 +0200

    Finalized JS for GUI

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 "use strict";
 
+// constant strings for comparison
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
 const TIE = 'Tie!';
 
+// DOM elements selection
 const playerOptions = document.querySelectorAll('#player div');
 const computerOptions = document.querySelectorAll('#computer div');
+const playerScoreText = document.querySelector('.player-right p');
+const computerScoreText = document.querySelector('.player-left p');
+const playerWinText = document.querySelector('#player-win-text');
+const computerWinText = document.querySelector('#pc-win-text');
 
+// global players score
+let playerWins = 0, computerWins = 0, gameEnded = false;
+
+
+
+// randomize computer choice
 let getComputerChoice = function() {
     let randomValue = Math.floor(Math.random() * 100) + 1;
     if (randomValue <= 30) return ROCK;
@@ -15,15 +27,17 @@ let getComputerChoice = function() {
     return SCISSORS;
 }
 
+// get player choice - console game
 function getPlayerChoice() {
     let userChoice = undefined;
     do {
         userChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors'").toLowerCase();
     } while (userChoice !== ROCK && userChoice !== PAPER && userChoice !== SCISSORS);
     return userChoice;
+
 }
 
-
+// function to compute game winner
 function getWinner(playerChoice , computerChoice) {
     if (playerChoice === computerChoice) return TIE;
     switch (playerChoice) {
@@ -51,50 +65,48 @@ function getWinner(playerChoice , computerChoice) {
     }
 }
 
-function computeGameWinner(playerWins, computerWins) {
-    if (playerWins > computerWins)
-        console.log("You win!");
-    else if (playerWins < computerWins)
-        console.log("The computer wins!");
-    else 
-        console.log("You and the computer tie!");
+function playRound(pChoice) {
+    if (gameEnded === true) return;
+    let playerChoice = pChoice.toLowerCase();
+    let computerChoice = getComputerChoice();
+    let playerWon = getWinner(playerChoice, computerChoice);
+    if (playerWon === TIE) {
+        console.log('Tie round!');
+        return;
+    }
+    if (playerWon) {
+        playerWins++;
+        playerScoreText.textContent = playerWins;
+        //console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+    }
+    else {
+        computerWins++;
+        computerScoreText.textContent = computerWins;
+        //console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    }
+    computeGameWinner();
 }
 
-function playGame() {
-    let playerWins = 0, computerWins = 0;
-    for (let i = 1; i <= 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let playerWon = getWinner(playerChoice, computerChoice);
-        if (playerWon === TIE) {
-            console.log('Tie round!');
-            continue;
-        }
-        if (playerWon) {
-            playerWins++;
-            console.log(`You win! ${playerChoice} beats ${computerChoice}`);
-        }
-        else {
-            computerWins++;
-            console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-        }
+function computeGameWinner() {
+    if (playerWins >= 5) {
+        playerWinText.classList.toggle('hidden');
+        gameEnded = true;
     }
-    computeGameWinner(playerWins, computerWins);
+    else if (computerWins >= 5) {
+        computerWinText.classList.toggle('hidden');
+        gameEnded = true;
+    }
 }
+
+
 
 
 //playGame();
-
 playerOptions.forEach( (choice) => {
     choice.addEventListener('click', (e) => {
-        console.log(e.target['alt'] === undefined? e.target['id'] : e.target['alt']);
         e.stopPropagation();
+        let pChoice = (e.target['alt'] === undefined)? e.target['id'].toLowerCase() : e.target['alt'].toLowerCase();
+        playRound(pChoice);
     });
 } );
 
-computerOptions.forEach( (choice) => {
-    choice.addEventListener('click', (e) => {
-        console.log(e.target['alt'] === undefined? e.target['id'] : e.target['alt']);
-        e.stopPropagation();
-    });
-} );
